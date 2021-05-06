056defa973b3d7ee440f36198c2f31bb1d3a3b69
Ana Studler <anistudler@gmail.com>
2021-05-05 21:12:39 -0300

    terminamos el captcha y parte del css
    Co-authored-by: Ana Laura <laurana1991@users.noreply.github.com>

--- js/captcha.js
+++ js/captcha.js
@@ -1,8 +1,21 @@
-"use strict"
+"use strict";
+/*  Buscamos al boton captcha en el DOM para crear el evento */
+let btn = document.querySelector("#btn-captcha").addEventListener("click", verificarCaptcha);
 
-let btn = document.querySelector("#btn-captcha");
-btn.addEventListener("click", verificarCaptcha);
 
+/* Creamos la funcion verificar */
 function verificarCaptcha() {
-    console.log("funciona")
+    /* Creamos varables buscando la informacion del captcha*/
+  let captcha = document.querySelector("#captcha").value;
+  let inputCaptcha = document.querySelector("#input-captcha").value;
+  let correcto = document.querySelector("#correcto");
+/* Hicimos un if para reconocer el captcha ingresado y su respuesta es correcto o incorrecto */
+  if (inputCaptcha === captcha) {
+      correcto.innerHTML = "Correcto";
+      console.log("correcto");
+  }
+  else {
+      correcto.innerHTML = "Incorrecto";
+      console.log("incorrecto");
+  }
 }
